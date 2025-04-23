c252f8dd57dac9acd472e5a8aa0f5cb6bc8e0d15
giovannagrigo <giovanna1grigo@gmail.com>
2025-04-22 22:30:32 -0300

    Adiciona o input e os botões na tela de AddTask

--- taskmanager/src/screens/AddTask.js
+++ taskmanager/src/screens/AddTask.js
@@ -1,16 +1,34 @@
-import { View } from "react-native";
-import { StyleSheet, Modal, TouchableWithoutFeedback } from "react-native";
+import { useState } from "react";
+import { View, StyleSheet, Modal, TouchableWithoutFeedback, TouchableOpacity, Text, TextInput } from "react-native";
+import commonStyles from "../commonStyles";
 
 export default function AddTask(props) {
-    return(
+    const [desc, setDesc] = useState("")
+    return (
         <Modal transparent={true} visible={props.isVisible} onRequestClose={props.onCancel} animationType="slide">
             <TouchableWithoutFeedback onPress={props.onCancel}>
                 <View style={styles.background}></View>
             </TouchableWithoutFeedback>
             <View style={styles.container}>
+                <Text style={styles.header}>Nova Tarefa</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Informe a Descrição"
+                    onChangeText={setDesc}
+                    value={desc}
+                />
+                <View style={styles.buttons}>
+                    <TouchableOpacity onPress={props.onCancel}>
+                        <Text style={styles.button}>Cancelar</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={props.save}>
+                        <Text style={styles.button}>Salvar</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
             <TouchableWithoutFeedback onPress={props.onCancel}>
-                    <View style={styles.background}></View>
+                <View style={styles.background}></View>
             </TouchableWithoutFeedback>
         </Modal>
     )
@@ -21,8 +39,32 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: 'rgba(0,0,0,0.7)'
     },
-    container:{
+    container: {
         backgroundColor: '#fff',
         flex: 1,
+    },
+    header:{
+        backgroundColor: commonStyles.colors.today,
+        color: commonStyles.colors.secondary,
+        textAlign: 'center',
+        padding: 15,
+        fontSize: 18
+    },
+    input: {
+        height: 40,
+        margin: 15,
+        backgorundColor: "#FFF",
+        borderWidth: 1,
+        borderColor: '#E3E3E3',
+        borderRadius: 6,
+    },
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end'
+    },
+    button: {
+        margin: 20,
+        marginRight: 30,
+        color: commonStyles.colors.today
     }
 })
