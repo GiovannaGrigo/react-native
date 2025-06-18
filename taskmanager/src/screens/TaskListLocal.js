7b4786d4245e2b91809ea84fbba1634949a09341
giovannagrigo <giovanna1grigo@gmail.com>
2025-06-17 22:19:52 -0300

    Requisições pelo MockAPI para CRUD

--- taskmanager/src/screens/TaskListLocal.js
+++ taskmanager/src/screens/TaskListLocal.js
@@ -1,13 +1,17 @@
 import { useEffect, useState } from "react"
-import { Text, View, StyleSheet, ImageBackground, TouchableOpacity, FlatList } from "react-native"
+import { Text, View, StyleSheet, ImageBackground, TouchableOpacity, FlatList, Alert } from "react-native"
+
 import Icon from "react-native-vector-icons/FontAwesome"
+
 import moment from "moment-timezone"
 import 'moment/locale/pt-br'
-import AsyncStorage from "@react-native-async-storage/async-storage"
-import axios from "axios"
+
 import todayImage from '../../assets/imgs/today.jpg'
-import Task from "../components/Task"
+
+import Task from '../components/Task'
+
 import AddTask from "./AddTask"
+import AsyncStorage from "@react-native-async-storage/async-storage"
 
 const taskDB = [
     {
@@ -30,17 +34,15 @@ const taskDB = [
     }
 ]
 
-export default function TaskList() {
+export default function TaskListLocal() {
+
     const [tasks, setTasks] = useState([])
-    const [showDoneTasks, setShowDoneTasks] = useState(true);
-    const [visibleTasks, setVisibleTasks] = useState([...tasks]);
-    const [showAddTask, setShowAddTask] = useState(false);
-
-    const today = moment().tz('America/Sao_Paulo').locale('pt-br').format('ddd, D [de] MMMM');
-
-    // const userTimeZone = moment.tz.guess(); // Detecta o fuso horario do dispositivo
-    // console.log(userTimeZone)
-    // const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
+    const [showDoneTasks, setShowDoneTasks] = useState(true)
+    const [visibleTasks, setVisibleTasks] = useState([...tasks])
+    const [showAddTask, setShowAddTask] = useState(false)
+    
+    const userTimeZone = moment.tz.guess(); // Detecta o fuso horario do dispositivo
+    const today = moment().tz('America/Sao_Paulo').locale('pt-br').format('ddd, D [de] MMMM')
 
     const [contador, setContador] = useState(0)
 
@@ -58,31 +60,20 @@ export default function TaskList() {
     }, [tasks])
 
     async function getTasks() {
-        try {
-            const response = await axios.get('https://68520d0d8612b47a2c0bf220.mockapi.io/Task')
-            setTasks(response.data)
-        } catch (error) {
-            console.error('Erro ao carregar os dados', error)
-        }
+        const tasksString = await AsyncStorage.getItem('tasksState')
+        const tasks = tasksString && JSON.parse(tasksString) || taskDB
+        setTasks(tasks)
     }
 
-    const toggleTask = async taskId => {
+    const toggleTask = taskId => {
         const taskList = [...tasks]
-        let taskUpdate = null;
         taskList.forEach(task => {
             if (task.id === taskId) {
-                task.doneAt = task.doneAt ? null : new Date();
-                taskUpdate = task;
+                task.doneAt = task.doneAt ? null : new Date()
             }
         });
+
         setTasks(taskList)
-
-        try {
-            const response = await axios.put(`https://68520d0d8612b47a2c0bf220.mockapi.io/Task/${taskUpdate.id}`, taskUpdate)
-        } catch (error) {
-            console.error('Erro ao atualizar tarefa', error)
-        }
-
         filterTasks()
     }
 
@@ -92,65 +83,50 @@ export default function TaskList() {
 
     const filterTasks = () => {
         let visibleTasks = null
-        if (showDoneTasks) {
+
+        if(showDoneTasks){
             visibleTasks = [...tasks]
+
         } else {
             const pending = task => task.doneAt === null
             visibleTasks = tasks.filter(pending)
         }
+
         setVisibleTasks(visibleTasks)
-        AsyncStorage.setItem('tasksState', JSON.stringify(tasks))
     }
 
-    const addTask = async newTask => {
-        if (!newTask.desc || !newTask.desc.trim()) {
+    const addTask = newTask => {
+        if(!newTask.desc || !newTask.desc.trim()){
             Alert.alert('Dados Inválidos', 'Descrição não informada!')
         }
 
         const tempTasks = [...tasks]
-
-        const taskAdd = {
-            id: getLastTaskId(),
+        tempTasks.push({
+            id: Math.random(),
             desc: newTask.desc,
             estimateAt: newTask.date,
             doneAt: null
-        }
-        tempTasks.push(taskAdd)
-
+        })
         setTasks(tempTasks)
         setShowAddTask(false)
-
-        try {
-            const response = await axios.post('https://68520d0d8612b47a2c0bf220.mockapi.io/Task', taskAdd)
-        } catch (error) {
-            console.error('Erro ao adicionar Tarefa', error)
-        }
 
         AsyncStorage.setItem('tasksState', JSON.stringify(tempTasks))
     }
 
-    const deleteTask = async id => {
-        // const tempTasks = tasks.filter(task => task.id !== id)
-        // setTasks(tempTasks)
-        // AsyncStorage.setItem('tasksState', JSON.stringify(tempTasks))
+    const deleteTask = id => {
+        const tempTasks = tasks.filter(task => task.id !== id)
+       setTasks(tempTasks)
 
-        try {
-            const response = await axios.delete(`https://68520d0d8612b47a2c0bf220.mockapi.io/Task/${id}`)
-        } catch (error) {
-            console.error('Erro ao excluir tarefa', error)
-        }
-
-        getTasks()
-    }
-
-    function getLastTaskId() {
-        return Math.max(...tasks.map(task => task.id)) + 1
+       AsyncStorage.setItem('tasksState', JSON.stringify(tempTasks))
     }
 
     return (
         <View style={styles.container}>
-            <AddTask isVisible={showAddTask} onCancel={() => setShowAddTask(false)}
-                onSave={addTask} />
+
+            <AddTask isVisible={showAddTask} 
+                onCancel={() => setShowAddTask(false)}
+                onSave={addTask}
+                />
 
             <ImageBackground source={todayImage} style={styles.background}>
                 <View style={styles.iconBar}>
@@ -168,8 +144,10 @@ export default function TaskList() {
             <View style={styles.taskList}>
                 <FlatList
                     data={visibleTasks}
-                    keyExtractor={item => `${item.id}`}  // "for" que percorre toda a lista e estabelece o valor pra cada item
-                    renderItem={({ item }) => <Task {...item} onToggleTask={toggleTask} onDelete={deleteTask} />}
+                    keyExtractor={item => `${item.id}`}
+                    renderItem={({ item }) => 
+                        <Task {...item} onToggleTask={toggleTask} onDelete={deleteTask}  />
+                }
                 />
             </View>
 
